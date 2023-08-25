d42ae3cc0cde02a86c90e8e2685bcddc4b46f5d3
trojvn <mrx.trojan@gmail.com>
2023-08-25 16:08:05 +0300

    feat: subprocess popen

--- routers/system.py
+++ routers/system.py
@@ -1,13 +1,23 @@
 import os
 import subprocess
+from dataclasses import dataclass
 
 from fastapi import APIRouter, HTTPException
 
 router = APIRouter(prefix="/system", tags=["system"])
 
 
+@dataclass
+class SubProcess:
+    cmd: str
+    process: subprocess.Popen
+
+
+processes: list[SubProcess] = []
+
+
 @router.post("/os")
-def os(cmd: str):
+async def os(cmd: str):
     try:
         result_code = os.system(cmd)
         return {"result": True, "code": result_code}
@@ -16,8 +26,16 @@ def os(cmd: str):
 
 
 @router.post("/subprocess/run")
-def subprocess_run(cmd: str):
+async def subprocess_run(cmd: str):
     result = subprocess.run(cmd, stdout=subprocess.PIPE, stderr=subprocess.PIPE)
     stdout = result.stdout.decode("utf-8")
     stderr = result.stderr.decode("utf-8")
     return {"result": True, "stdout": stdout, "stderr": stderr}
+
+
+@router.post("/subprocess/popen")
+async def subprocess_popen(cmd: str):
+    result = subprocess.Popen(cmd, stdout=subprocess.PIPE, stderr=subprocess.PIPE)
+    process = SubProcess(cmd, result)
+    processes.append(process)
+    return {"result": True, "stdout": result.stdout, "stderr": result.stderr}
