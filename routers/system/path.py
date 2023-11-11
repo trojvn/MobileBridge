f6c624fa0a2942cc87c9438dc32c1952ba41b5f1
trojvn <mrx.trojan@gmail.com>
2023-11-11 22:31:00 +0300

    walk, move

--- routers/system/path.py
+++ routers/system/path.py
@@ -2,17 +2,17 @@ import contextlib
 import os
 import shutil
 from pathlib import Path
-from typing import Literal
+from typing import Literal, Optional
 
 from fastapi import APIRouter
 
 router = APIRouter(prefix="/system/path", tags=["path"])
 
-Actions = Literal["mkdir", "remove", "exists"]
+Actions = Literal["mkdir", "remove", "move", "exists"]
 
 
 @router.post("/")
-async def system_path(action: Actions, path: str):
+async def system_path(action: Actions, path: str, dst_path: Optional[str] = None):
     p = Path(path)
     if action == "mkdir":
         p.parent.parent.mkdir(exist_ok=True)
@@ -27,4 +27,20 @@ async def system_path(action: Actions, path: str):
     elif action == "exists":
         if p.exists():
             return {"result": True}
-        return {"result": False}
+    elif action == "move":
+        while True:
+            with contextlib.suppress(Exception):
+                shutil.move(path, Path(dst_path))
+                return {"result": True}
+    elif action == "walk":
+        files = []
+        dirs = []
+        for item in p.iterdir():
+            if item.is_file():
+                files.append(str(item))
+            if item.is_dir():
+                dirs.append(str(item))
+        a = files + dirs
+        return {"result": True, "files": files, "dirs": dirs, "all": a}
+
+    return {"result": False}
