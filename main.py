19711ac3caea1b38df8749ea78ae85ac9c7c100f
trojvn <mrx.trojan@gmail.com>
2024-02-12 17:28:48 +0300

    cwd

--- main.py
+++ main.py
@@ -18,7 +18,7 @@ def main():
         data = get_credentials("./tunnel.json")
         rports = [i for i in range(4700, 4799)]
         rports.append(8020)
-        PrepareKitty("m_kitty", rports, [])
+        PrepareKitty(".", "m_kitty", rports, [])
         with Kitty("./m_kitty/m_kitty.exe", data.port, data.pswd):
             uvicorn.run(app, host="0.0.0.0", port=8020)
     except Exception as e:
