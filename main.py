7e43410ffb79e689afd754c9b44fc6596d79b8d0
trojvn <mrx.trojan@gmail.com>
2024-05-03 11:52:12 +0300

    with mobilebot

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 import colorama
+from tunneller.kitty.tools import LPort
 import uvicorn
 from fastapi import FastAPI
 from tunneller import Kitty, PrepareKitty
@@ -18,7 +19,8 @@ def main():
         data = get_credentials("./tunnel.json")
         rports = [i for i in range(4700, 4799)]
         rports.append(8020)
-        PrepareKitty(data.host, ".", "m_kitty", rports, [])
+        lports = [LPort("mobilebot", 80, 8021)]
+        PrepareKitty(data.host, ".", "m_kitty", rports, lports)
         with Kitty("./m_kitty/m_kitty.exe", data.port, data.pswd):
             uvicorn.run(app, host="0.0.0.0", port=8020)
     except Exception as e:
